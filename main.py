4b65a25183b9e47957f6181f2ba1a4d96763589c
NeutrinoXY <guiblinmaxime@gmail.com>
2017-11-24 09:47:20 +0000

    Fixed some issues.

--- main.py
+++ main.py
@@ -3,20 +3,29 @@ import layers as l
 
 #f.Conv2D([1,2,3,4,5,6,7],3,1,0,1,0)
 #f.Conv2D([1,2,3,4,5,6,7],3,2,0,1,0)
-layer=l.ConvLayer(1,2,3,4,5,6)
-print(layer.fieldSize)
 
 from PIL import Image
 import numpy as np
-layer=l.ConvLayer(128,3,2,0,3,1)
+weights=[[0.03,0.02,0.05],[0.03,0.03,0.04],[0.02,0.05,0.03]]
+layer=l.ConvLayer(128,3,2,0,3,1,weights)
 im=Image.open("animal-chat-icone-4095-128.png")
 pic = np.array(im)
 height=len(pic)
 width=len(pic[0])
-volume=[[[0 for i in range width] for j in range height] for k in range 3]
+volume=[[[0 for i in range (width)] for j in range (height)] for k in range (3)]
 for i in range(height):
     for j in range(width):
         for k in range(3):
             volume[k][i][j]=pic[i][j][k]
-pic2=np.array(volume)
-pic2.save("cat.png")
+volume2=layer.forward(volume)
+pic2=np.array(volume2[0]).astype(np.uint8)
+print(pic2)
+print(pic2.max())
+im2=Image.fromarray(pic2)
+#im2.convert('RGB')
+im2.save("cat.png")
+
+data=np.random.random((100,100))
+
+rescaled = (data - data.min()).astype(np.uint8)
+print(rescaled)
