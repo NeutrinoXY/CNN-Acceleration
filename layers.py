b68045c10e7df544c3123304ef9458905ea33b70
NeutrinoXY <guiblinmaxime@gmail.com>
2017-11-24 09:47:43 +0000

    Fixed some issues.

--- layers.py
+++ layers.py
@@ -1,21 +1,22 @@
 class ConvLayer():
-    def __init__(self,inputSize,fieldSize,stride,zeroPadding,inputChannels,outputChannels):
+    def __init__(self,inputSize,fieldSize,stride,zeroPadding,inputChannels,outputChannels,weights):
         self.inputSize=inputSize
         self.fieldSize=fieldSize
         self.stride=stride
         self.zeroPadding=zeroPadding
         self.inputChannels=inputChannels
         self.outputChannels=outputChannels
+        self.weights=weights
+        self.nbNeurons=int((self.inputSize-self.fieldSize+2*self.zeroPadding)/self.stride + 1)
 
-    def forward(inputVolume):
-        nbNeurons=(len(inputVolume)-fieldSize+2*zeroPadding)/stride + 1
-        tab=[[0 for i in range(nbNeurons)] for j in range(nbNeurons)]
-        for i in range(outputChannels):
-            for j in range(nbNeurons):
-                for k in range(nbNeurons):
-                    for l in range(fieldSize ):
-                        for m in range(fieldSize):
-                            for n in range(inputChannels):
-                                tab[i][j][k]+=inputVolume[n][j*stride+l][k*stride+m]*weights[l][m]
+    def forward(self,inputVolume):
+        tab=[[[0 for i in range(self.nbNeurons)] for j in range(self.nbNeurons)] for k in range(self.outputChannels)]
+        for i in range(self.outputChannels):
+            for j in range(self.nbNeurons):
+                for k in range(self.nbNeurons):
+                    for l in range(self.fieldSize ):
+                        for m in range(self.fieldSize):
+                            for n in range(self.inputChannels):
+                                tab[i][j][k]+=inputVolume[n][j*self.stride+l][k*self.stride+m]*self.weights[l][m]
         return tab
 
