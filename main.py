fe4aaacf1c72d54ddac8d05978b9a1b51ebeb244
NeutrinoXY <guiblinmaxime@gmail.com>
2017-11-30 17:26:14 +0000

    void

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ from PIL import Image
 import numpy as np
 weights1=[[0.03,0.02,0.04,0.01],[0.02,0.03,0.03,0.02],[0.02,0.05,0.02,0.01],[0.01,0.01,0.03,0.05]]
 weights2=[[0.1,0.2],[0.3,0.35]]
-layer1=l.ConvLayer(452,4,2,0,3,1,weights)
-layer2=l.ConvLayer(225,2,5,0,3,1,weights)
+layer1=l.ConvLayer(452,4,2,0,3,1,weights1)
+layer2=l.ConvLayer(225,2,5,0,3,1,weights2)
 im=Image.open("cat_origin.jpeg")
 pic = np.array(im)
 height=len(pic)
@@ -19,7 +19,7 @@ for i in range(height):
     for j in range(width):
         for k in range(3):
             volume[k][i][j]=pic[i][j][k]
-volume2=layer.forward(volume)
+volume2=layer1.forward(volume)
 pic2=np.array(volume2[0]).astype(np.uint8)
 im2=Image.fromarray(pic2)
 #im2.convert('RGB')
